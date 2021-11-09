c7949d9aa459d17cc1ddd73e9a7b502842edb731
PavanBandichode <pavan.bandichode@gmail.com>
2021-11-09 14:03:47 +0530

    arguments and return

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -1,7 +1,10 @@
 /*
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
-
+function sayHello(name){
+alert(`hello ${name}!`);
+}
+sayHello("pavan");
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -10,7 +13,11 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
-
+function getFullName(firstName,lastName){
+  return`${firstName} ${lastName}`;
+  }
+  getFullName("pavan","bandichode");
+  getFullName("harish","stya");
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
 the sum of both numbers. If user passes any data other then numbers alert a message saying `Enter Valid Input`
@@ -20,6 +27,10 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+function addTwoNumbers(firstNum,secondNum){
+  return`${firstNum+secondNum}`;
+  }
+  addTwoNumbers(10,20);
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -32,7 +43,21 @@ calc(10, 20, 'add'); // 30
 calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
-
+function clac(numA,numB,operation){
+  switch(operation){
+    case add:
+      add= numA + numB;
+    return add;
+    case mul:
+      mul= numA * numB;
+    return mul;
+    case sub:
+      sub= numA - numB;
+    return sub;
+    default:
+      alert(`Enter Valid Input`);
+  }
+}
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -41,8 +66,25 @@ on if the year id leap year or not.
 isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
-
+function isLeapYear(year){
+    // let year=+prompt("enter a year");
+    if(year%4==0){
+      // alert(`${year} is a leap year`);
+      return true;
+    }else{
+      // alert(`${year} is not a leap year`);
+      return false;
+    }
+  }
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+function getFactorial(number){
+  let factorial=1;
+  for(let i=number;i>=1;i--){
+ factorial =factorial*i;
+  }
+  return factorial;
+}
+getFactorial(5);
